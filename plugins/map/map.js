415cb8cacc4eef1f259acb5b8e00deee81ee605e
Eric Dobbs <eric@dobbse.net>
2023-07-01 19:15:41 -0600

    upgrade to wiki 0.30.0 & add Design Vernacular

--- plugins/map/map.js
+++ plugins/map/map.js
@@ -7,12 +7,14 @@
    * Licensed under the MIT license.
    * https://github.com/fedwiki/wiki-plugin-map/blob/master/LICENSE.txt
    */
-  var bind, emit, escape, feature, htmlDecode, lineup, marker, parse, resolve;
+  var bind, emit, escape, feature, htmlDecode, lineup, marker, page, parse, resolve, useLineupMarkers, usePageMarkers;
 
+  // which markers types
+  usePageMarkers = false;
+  useLineupMarkers = false;
   escape = function escape(line) {
     return line.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
   };
-
   resolve = function resolve(text) {
     if (typeof wiki !== "undefined" && wiki !== null) {
       return wiki.resolveLinks(text, escape);
@@ -20,30 +22,24 @@
       return escape(text).replace(/\[\[.*?\]\]/g, '<internal>').replace(/\[.*?\]/g, '<external>');
     }
   };
-
   htmlDecode = function htmlDecode(escapedText) {
     var doc;
     doc = new DOMParser().parseFromString(escapedText, "text/html");
     return doc.documentElement.textContent;
   };
-
   marker = function marker(text) {
     var decimal, deg, m, nautical;
-
     deg = function deg(m) {
       var num;
       num = +m[0] + m[1] / 60 + (m[2] || 0) / 60 / 60;
-
       if (m[3].match(/[SW]/i)) {
         return -num;
       } else {
         return num;
       }
     };
-
     decimal = /^(-?\d{1,3}\.?\d*)[, ] *(-?\d{1,3}\.?\d*)\s*(.*)$/;
     nautical = /^(\d{1,3})°(\d{1,2})'(\d*\.\d*)?"?([NS]) (\d{1,3})°(\d{1,2})'(\d*\.\d*)?"?([EW]) (.*)$/i;
-
     if (m = decimal.exec(text)) {
       return {
         lat: +m[1],
@@ -51,7 +47,6 @@
         label: resolve(m[3])
       };
     }
-
     if (m = nautical.exec(text)) {
       return {
         lat: deg(m.slice(1, 5)),
@@ -59,13 +54,10 @@
         label: resolve(m[9])
       };
     }
-
     return null;
   };
-
   lineup = function lineup($item) {
     var candidates, div, i, len, lineupMarkers, who;
-
     if (typeof wiki === "undefined" || wiki === null) {
       return [{
         lat: 51.5,
@@ -73,61 +65,79 @@
         label: 'North Greenwich'
       }];
     }
-
     lineupMarkers = [];
     candidates = $(".item:lt(".concat($('.item').index($item), ")"));
-
-    if ((who = candidates.filter(".marker-source")).size()) {
+    if ((who = candidates.filter(".marker-source")).length) {
       for (i = 0, len = who.length; i < len; i++) {
         div = who[i];
         lineupMarkers = lineupMarkers.concat(div.markerData());
       }
     }
-
     return lineupMarkers;
   };
-
+  page = function page($item) {
+    var candidates, div, i, len, pageMarkers, who;
+    if (typeof wiki === "undefined" || wiki === null) {
+      return [{
+        lat: 51.5,
+        lon: 0.0,
+        label: 'North Greenwich'
+      }];
+    }
+    pageMarkers = [];
+    candidates = $item.siblings();
+    if ((who = candidates.filter(".marker-source")).length) {
+      for (i = 0, len = who.length; i < len; i++) {
+        div = who[i];
+        pageMarkers = pageMarkers.concat(div.markerData());
+      }
+    }
+    return pageMarkers;
+  };
   parse = function parse(item, $item) {
-    var boundary, captions, hint, hints, i, len, line, lineupMarkers, m, markers, overlays, ref, result, text, tools, weblink;
-    text = item.text; // parsing the plugin text in context of any frozen items
-
+    var boundary, captions, hint, hints, i, len, line, lineupMarkers, m, markers, overlays, pageMarkers, ref, result, text, tools, weblink;
+    text = item.text;
+    // parsing the plugin text in context of any frozen items
     captions = [];
     markers = [];
     lineupMarkers = null;
+    pageMarkers = null;
     overlays = null;
     boundary = null;
     weblink = null;
     tools = {};
-
+    usePageMarkers = false;
+    useLineupMarkers = false;
     if (item.frozen) {
       markers = markers.concat(item.frozen);
     }
-
     ref = text.split(/\n/);
-
     for (i = 0, len = ref.length; i < len; i++) {
       line = ref[i];
-
       if (m = marker(line)) {
         if (weblink != null) {
           m.weblink = weblink;
         }
-
         markers.push(m);
       } else if (m = /^BOUNDARY *(.*)?$/.exec(line)) {
         hints = (hint = marker(m[1])) ? [hint] : [];
-
         if (boundary == null) {
           boundary = markers.concat([]);
         }
-
         boundary = boundary.concat(hints);
       } else if (/^LINEUP/.test(line)) {
         tools['freeze'] = true;
-        lineupMarkers = lineup($item);
-
+        useLineupMarkers = true;
         if (!item.frozen) {
+          lineupMarkers = lineup($item);
           markers = markers.concat(lineupMarkers);
+        }
+      } else if (/^PAGE/.test(line)) {
+        tools['freeze'] = true;
+        usePageMarkers = true;
+        if (!item.frozen) {
+          pageMarkers = page($item);
+          markers = markers.concat(pageMarkers);
         }
       } else if (m = /^WEBLINK *(.*)$/.exec(line)) {
         weblink = m[1];
@@ -143,44 +153,40 @@
       } else {
         captions.push(resolve(line));
       }
-    } // remove any duplicate markers
-
-
+    }
+    // remove any duplicate markers
     markers = Array.from(new Set(markers.map(JSON.stringify))).map(JSON.parse);
-
     if (lineupMarkers) {
       lineupMarkers = Array.from(new Set(lineupMarkers.map(JSON.stringify))).map(JSON.parse);
     }
-
+    if (pageMarkers) {
+      pageMarkers = Array.from(new Set(pageMarkers.map(JSON.stringify))).map(JSON.parse);
+    }
     if (boundary == null) {
       boundary = markers;
     }
-
     result = {
       markers: markers,
       caption: captions.join('<br>'),
       boundary: boundary
     };
-
     if (lineupMarkers) {
       result.lineupMarkers = lineupMarkers;
     }
-
+    if (pageMarkers) {
+      result.pageMarkers = pageMarkers;
+    }
     if (Object.keys(tools).length > 0) {
       result.tools = tools;
     }
-
     if (weblink != null) {
       result.weblink = weblink;
     }
-
     if (overlays != null) {
       result.overlays = overlays;
     }
-
     return result;
   };
-
   feature = function feature(marker) {
     return {
       type: 'Feature',
@@ -193,53 +199,45 @@
       }
     };
   };
-
   emit = function emit($item, item) {
-    var boundary, caption, lineupMarkers, markers, overlays, showing, tools, weblink;
-
+    var boundary, caption, lineupMarkers, markers, overlays, pageMarkers, showing, tools, weblink;
+    // announce our capability to produce markers in native and geojson format
     var _parse = parse(item, $item);
-
     caption = _parse.caption;
     markers = _parse.markers;
     lineupMarkers = _parse.lineupMarkers;
+    pageMarkers = _parse.pageMarkers;
     boundary = _parse.boundary;
     weblink = _parse.weblink;
     overlays = _parse.overlays;
     tools = _parse.tools;
-    // announce our capability to produce markers in native and geojson format
     $item.addClass('marker-source');
     showing = [];
-
     $item.get(0).markerData = function () {
       var opened;
       opened = showing.filter(function (s) {
         return s.leaflet._popup._isOpen;
       });
-
       if (opened.length) {
         return opened.map(function (s) {
           return s.marker;
         });
       } else {
-        return parse(item).markers;
+        return markers;
       }
     };
-
     $item.get(0).markerGeo = function () {
       return {
         type: 'FeatureCollection',
-        features: parse(item).markers.map(feature)
+        features: markers.map(feature)
       };
     };
-
     if (!$("link[href='https://unpkg.com/leaflet@1.7.1/dist/leaflet.css']").length) {
       $('<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css">').appendTo("head");
     }
-
     if (!$("link[href='/plugins/map/map.css']").length) {
       $('<link rel="stylesheet" href="/plugins/map/map.css" type="text/css">').appendTo("head");
     }
-
     return wiki.getScript("https://unpkg.com/leaflet@1.7.1/dist/leaflet.js", function () {
       var bounds, freezeControl, i, len, map, mapDiv, mapId, o, openWeblink, p, ref, showMarkers, tile, tileCredits, update;
       mapId = "map-".concat(Math.floor(Math.random() * 1000000));
@@ -247,7 +245,6 @@
       map = L.map(mapId, {
         scrollWheelZoom: false
       });
-
       update = function update() {
         wiki.pageHandler.put($item.parents('.page:first'), {
           type: 'edit',
@@ -255,18 +252,15 @@
           item: item
         });
         return wiki.doPlugin($item.empty(), item);
-      }; // add locate control
-
-
+      };
+      // add locate control
       if (tools != null ? tools.locate : void 0) {
         if (!$("link[href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css']").length) {
           $('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">').appendTo("head");
         }
-
         if (!$("link[href='https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.72.0/dist/L.Control.Locate.min.css'")) {
           $('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.72.0/dist/L.Control.Locate.min.css">').appendTo("head");
         }
-
         wiki.getScript("https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.72.0/dist/L.Control.Locate.min.js", function () {
           return L.control.locate({
             position: 'topleft',
@@ -275,14 +269,12 @@
             drawMarker: false
           }).addTo(map);
         });
-      } // add search control
-
-
+      }
+      // add search control
       if (tools != null ? tools.search : void 0) {
         if (!$("link[href='https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css']").length) {
           $('<link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />').appendTo("head");
         }
-
         wiki.getScript("https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js", function () {
           var geocoder;
           return geocoder = L.Control.geocoder({
@@ -296,31 +288,27 @@
               lat: e.geocode.center.lat,
               lon: e.geocode.center.lng
             });
-
             if (boundary.length > 1) {
               bounds = new L.LatLngBounds([function () {
                 var i, len, results;
                 results = [];
-
                 for (i = 0, len = boundary.length; i < len; i++) {
                   p = boundary[i];
                   results.push([p.lat, p.lon]);
                 }
-
                 return results;
               }()]);
+              bounds = bounds.pad(0.3);
               map.flyToBounds(bounds);
             } else if (boundary.length === 1) {
               p = boundary[0];
               map.flyTo([p.lat, p.lon], item.zoom || 13);
             }
-
             item.text += "\n".concat(e.geocode.center.lat.toFixed(7), ", ").concat(e.geocode.center.lng.toFixed(7), " ").concat(e.geocode.name);
             return update();
           }).addTo(map);
         });
       }
-
       if (tools != null ? tools.freeze : void 0) {
         freezeControl = L.Control.extend({
           options: {
@@ -329,50 +317,51 @@
           onAdd: function onAdd(map) {
             var container, newMarkerGroup, newMarkers;
             container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
-            container.innerHTML = "<a class=\"leaflet-bar-part leaflet-bar-part-single\" href=\"#\" style=\"outline: currentcolor none medium;\">\n  <span style=".concat(typeof item.frozen === "function" ? item.frozen({
-              'color: black;': 'color: blue;'
-            }) : void 0, ">\u2744\uFE0E</span>\n</a>");
+            container.innerHTML = "<a class=\"leaflet-bar-part leaflet-bar-part-single\" href=\"#\" style=\"outline: currentcolor none medium;\">\n  <span style=\"".concat(item.frozen ? 'color: #5792FF;' : 'color: blue;', "\">\u2744\uFE0E</span>\n</a>");
             newMarkers = [];
             newMarkerGroup = null;
-
             container.onclick = function (e) {
               var toFreeze;
-
               if (e.shiftKey) {
                 e.preventDefault();
                 e.stopPropagation();
-
                 if (item.frozen) {
                   delete item.frozen;
                   return update();
                 }
               } else {
+                if (usePageMarkers) {
+                  pageMarkers = page($item);
+                }
                 toFreeze = [];
-
                 if (item.frozen) {
-                  toFreeze = Array.from(new Set(item.frozen.concat(lineupMarkers).map(JSON.stringify))).map(JSON.parse);
+                  toFreeze = Array.from(new Set(item.frozen.concat(lineupMarkers || pageMarkers).map(JSON.stringify))).map(JSON.parse);
                 } else {
-                  toFreeze = lineupMarkers;
-                } // only update if there realy is something new to freeze or it has changed...
-
-
+                  toFreeze = lineupMarkers || pageMarkers;
+                }
+                // only update if there realy is something new to freeze or it has changed...
                 if (item.frozen && toFreeze.length !== item.frozen.length || !item.frozen && toFreeze.length > 0) {
                   //(!item.frozen and toFreeze.length > 0) or toFreeze.length != item.frozen.length
                   item.frozen = toFreeze;
                   return update();
                 }
               }
-            }; // mouse over will show any extra markers that will be added on a re-freeze.
+            };
 
-
+            // mouse over will show any extra markers that will be added on a re-freeze.
             container.addEventListener('mouseenter', function (e) {
               var bounds, l, m, p, tmpBoundary;
+              if (usePageMarkers) {
+                pageMarkers = page($item);
+              }
+              if (useLineupMarkers) {
+                lineupMarkers = lineup($item);
+              }
               m = new Set(markers.map(JSON.stringify));
-              l = new Set(lineupMarkers.map(JSON.stringify));
+              l = new Set((lineupMarkers || pageMarkers).map(JSON.stringify));
               newMarkers = Array.from(new Set(Array.from(l).filter(function (x) {
                 return !m.has(x);
               }))).map(JSON.parse);
-
               if (newMarkers.length > 0) {
                 newMarkerGroup = L.layerGroup().addTo(map);
                 newMarkers.forEach(function (mark) {
@@ -382,35 +371,31 @@
                 bounds = new L.LatLngBounds([function () {
                   var i, len, results;
                   results = [];
-
                   for (i = 0, len = tmpBoundary.length; i < len; i++) {
                     p = tmpBoundary[i];
                     results.push([p.lat, p.lon]);
                   }
-
                   return results;
                 }()]);
+                bounds = bounds.pad(0.3);
                 return map.flyToBounds(bounds);
               }
             });
             container.addEventListener('mouseleave', function (e) {
               var bounds, p;
-
               if (newMarkers.length > 0) {
                 newMarkerGroup.remove();
-
                 if (boundary.length > 1) {
                   bounds = new L.LatLngBounds([function () {
                     var i, len, results;
                     results = [];
-
                     for (i = 0, len = boundary.length; i < len; i++) {
                       p = boundary[i];
                       results.push([p.lat, p.lon]);
                     }
-
                     return results;
                   }()]);
+                  bounds = bounds.pad(0.3);
                   return map.flyToBounds(bounds);
                 } else if (boundary.length === 1) {
                   p = boundary[0];
@@ -423,12 +408,11 @@
             return container;
           },
           onRemove: function onRemove(map) {}
-        }); // Nothing to do here...
-
+        });
+        // Nothing to do here...
         map.addControl(new freezeControl());
-      } // stop dragging the map from propagating and dragging the page item.
-
-
+      }
+      // stop dragging the map from propagating and dragging the page item.
       mapDiv = L.DomUtil.get("".concat(mapId));
       L.DomEvent.disableClickPropagation(mapDiv);
       map.doubleClickZoom.disable();
@@ -439,15 +423,15 @@
           item.text += "\n".concat(e.latlng.lat.toFixed(7), ", ").concat(e.latlng.lng.toFixed(7));
           return update();
         }
-      }); // select tiles, default to OSM
-
+      });
+      // select tiles, default to OSM
       tile = item.tile || "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
       tileCredits = item.tileCredits || '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
       L.tileLayer(tile, {
         attribution: tileCredits
       }).addTo(map);
-      ref = overlays || []; // add specialized map overlays
-
+      ref = overlays || [];
+      // add specialized map overlays
       for (i = 0, len = ref.length; i < len; i++) {
         o = ref[i];
         L.imageOverlay(o.url, o.bounds, {
@@ -455,26 +439,19 @@
           interactive: true
         }).addTo(map);
       }
-
       openWeblink = function openWeblink(e) {
         var link;
-
         if (!(link = e.target.options.weblink)) {
           return;
         }
-
         return window.open(link.replace(/\{LAT}/, e.latlng.lat).replace(/\{(LON|LNG)}/, e.latlng.lng));
       };
-
       showMarkers = function showMarkers(markers) {
         var j, len1, markerLabel, mkr, p, results;
-
         if (!markers) {
           return;
         }
-
         results = [];
-
         for (j = 0, len1 = markers.length; j < len1; j++) {
           p = markers[j];
           markerLabel = htmlDecode(wiki.resolveLinks(p.label));
@@ -486,34 +463,30 @@
             marker: p
           }));
         }
-
         return results;
-      }; // add markers on the map
-
-
-      showMarkers(markers); // center map on markers or item properties
-
+      };
+      // add markers on the map
+      showMarkers(markers);
+      // center map on markers or item properties
       if (boundary.length > 1) {
         bounds = new L.LatLngBounds([function () {
           var j, len1, results;
           results = [];
-
           for (j = 0, len1 = boundary.length; j < len1; j++) {
             p = boundary[j];
             results.push([p.lat, p.lon]);
           }
-
           return results;
         }()]);
+        bounds = bounds.pad(0.3);
         map.fitBounds(bounds);
       } else if (boundary.length === 1) {
         p = boundary[0];
         map.setView([p.lat, p.lon], item.zoom || 13);
       } else {
         map.setView(item.latlng || item.latLng || [40.735383, -73.984655], item.zoom || 13);
-      } // announce our capability to produce a region
-
-
+      }
+      // announce our capability to produce a region
       $item.addClass('region-source');
       return $item.get(0).regionData = function () {
         var region;
@@ -527,20 +500,17 @@
       };
     });
   };
-
   bind = function bind($item, item) {
-    return $item.dblclick(function () {
+    return $item.on('dblclick', function () {
       return wiki.textEditor($item, item);
     });
   };
-
   if (typeof window !== "undefined" && window !== null) {
     window.plugins.map = {
       emit: emit,
       bind: bind
     };
   }
-
   if (typeof module !== "undefined" && module !== null) {
     module.exports = {
       marker: marker,
